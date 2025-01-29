8355bea793139ea5f34453f32bec9bb11f35cc83
Julien Roussel <julien.roussel@gmail.com>
2025-01-29 19:40:01 +0100

    feat: use mantine provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
 import { useState, useMemo, useCallback } from 'react';
-import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import { Effect } from 'effect';
 
@@ -15,26 +16,14 @@ function App() {
   const increment = useCallback(() => Effect.runSync(task), [task]);
 
   return (
-    <>
+    <MantineProvider>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={increment}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button onClick={increment}>count is {count}</button>
+    </MantineProvider>
   );
 }
 
