2dc69e424d624f3411cd5bbd23257df6c4466cf1
Julien Roussel <julien.roussel@gmail.com>
2025-01-29 19:34:50 +0100

    chore: remove vanilla css and use postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import '@mantine/core/styles.css';
 import { useState, useMemo, useCallback } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
 import { Effect } from 'effect';
 
 function App() {
