8645e8ded36409002c6138e302f526bd01c2a494
Julien Roussel <julien.roussel@gmail.com>
2025-02-01 16:12:49 +0100

    feat: better integration between Mantine NavBar and React Router Link

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -6,34 +6,32 @@ import { NavBarFooter } from './NavBarFooter';
 export const NavBar = () => (
   <AppShell.Navbar p="md">
     <AppShell.Section grow component={ScrollArea}>
-      <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }}>
-        <NavLink
-          href="#required-for-focus"
-          label="Home"
-          leftSection={<IconHome2 size={16} stroke={1.5} />}
-          rightSection={
-            <IconChevronRight
-              size={12}
-              stroke={1.5}
-              className="mantine-rotate-rtl"
-            />
-          }
-        />
-      </Link>
-      <Link to="/quiz" style={{ color: 'inherit', textDecoration: 'inherit' }}>
-        <NavLink
-          href="#required-for-focus"
-          label="Quiz"
-          leftSection={<IconSchool size={16} stroke={1.5} />}
-          rightSection={
-            <IconChevronRight
-              size={12}
-              stroke={1.5}
-              className="mantine-rotate-rtl"
-            />
-          }
-        />
-      </Link>
+      <NavLink
+        component={Link}
+        to="/"
+        label="Home"
+        leftSection={<IconHome2 size={16} stroke={1.5} />}
+        rightSection={
+          <IconChevronRight
+            size={12}
+            stroke={1.5}
+            className="mantine-rotate-rtl"
+          />
+        }
+      />
+      <NavLink
+        component={Link}
+        to="/quiz"
+        label="Quiz"
+        leftSection={<IconSchool size={16} stroke={1.5} />}
+        rightSection={
+          <IconChevronRight
+            size={12}
+            stroke={1.5}
+            className="mantine-rotate-rtl"
+          />
+        }
+      />
     </AppShell.Section>
     <AppShell.Section>
       <NavBarFooter />
