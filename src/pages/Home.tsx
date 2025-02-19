373358619c2605d7faf88f30760608a2b0b442f8
Julien Roussel <julien.roussel@gmail.com>
2025-02-20 00:52:32 +0100

    feat: use Mantine Grid to have card ribbon maximal height

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Title, Text, Space, Anchor } from '@mantine/core';
+import { Title, Text, Space, Anchor, Grid } from '@mantine/core';
 import { usePageTracking } from '../hooks/usePageTracking';
 import { StackPicker } from '../components/StackPicker';
 import { stacks } from '../types/stacks';
@@ -12,42 +12,55 @@ export const Home = () => {
   usePageTracking();
 
   return (
-    <div>
-      <Title order={2}>Welcome to MemDeck</Title>
-      <Space h="lg" />
-      <Text>
-        Hope these tools help you level up your memorized deck stack learning.
-        Need a hand or think something's missing? Hit me up on{' '}
-        <Anchor href={GITHUB_URL} target="_blank" underline="never">
-          Github
-        </Anchor>
-        !
-      </Text>
-      {stack === '' && (
-        <>
+    <div className="fullMantineContainerHeight">
+      <Grid
+        gutter={0}
+        overflow="hidden"
+        style={{
+          display: 'grid',
+          height: '100%',
+        }}
+      >
+        <Grid.Col span={12}>
+          <Title order={2}>Welcome to MemDeck</Title>
           <Space h="lg" />
           <Text>
-            Hey there, first-timer! Pick your favorite memorized deck stack
-            below to unlock all the cool features. You can switch it up anytime
-            using the selector at the bottom of the menu.
+            Hope these tools help you level up your memorized deck stack
+            learning. Need a hand or think something's missing? Hit me up on{' '}
+            <Anchor href={GITHUB_URL} target="_blank" underline="never">
+              Github
+            </Anchor>
+            !
           </Text>
-          <Space h="lg" />
-          <StackPicker />
-        </>
-      )}
-      {stack !== '' && (
-        <>
-          <Space h="lg" />
-          <Text>
-            Your selected stack is{' '}
-            <Text span fw={700}>
-              {stacks[stack]?.name}
-            </Text>
-          </Text>
-          <Space h="lg" />
-          <CardSpread items={[...stacks[stack].order]} degree={0.5} />
-        </>
-      )}
+          {stack === '' && (
+            <>
+              <Space h="lg" />
+              <Text>
+                Hey there, first-timer! Pick your favorite memorized deck stack
+                below to unlock all the cool features. You can switch it up
+                anytime using the selector at the bottom of the menu.
+              </Text>
+              <Space h="lg" />
+              <StackPicker />
+            </>
+          )}
+        </Grid.Col>
+        <Grid.Col span={12} style={{ height: '100%' }}>
+          {stack !== '' && (
+            <>
+              <Space h="lg" />
+              <Text>
+                Your selected stack is{' '}
+                <Text span fw={700}>
+                  {stacks[stack]?.name}
+                </Text>
+              </Text>
+              <Space h="lg" />{' '}
+              <CardSpread items={[...stacks[stack].order]} degree={0.5} />
+            </>
+          )}
+        </Grid.Col>
+      </Grid>
     </div>
   );
 };
