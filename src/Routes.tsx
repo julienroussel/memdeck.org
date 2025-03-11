656d0c8eb26a4ec078e41eb4507a29ff5f88bb57
Julien Roussel <julien.roussel@gmail.com>
2025-03-11 09:21:00 +0100

    fix: ACAAN typo (#55)

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,7 +2,7 @@ import { Routes as RouterRoutes, Route as RouterRoute } from 'react-router';
 import { Flashcard } from './pages/Flashcard/Flashcard';
 import { Home } from './pages/Home';
 import { Shuffle } from './pages/Shuffle';
-import { Acaan } from './pages/ACANN';
+import { Acaan } from './pages/ACAAN';
 import { Resources } from './pages/Resources';
 import { Toolbox } from './pages/Toolbox';
 
